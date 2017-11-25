ebaca434c2dd41711dc8c669dad623a6adf4c2eb
Corey Fonseca <cfonseca@blueport.com>
2017-11-25 15:04:16 -0500

    Added and imported flask-socketio extension.

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
+from flask.ext.socketio import SocketIO, emit, join_room
+
+
 # need some sort of auth library
 app = Flask(__name__)
 
@@ -9,7 +12,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# /chat/
+# redirect to /chat/general
+# requires login
 
+# /chat/<roomname>
+# redirect to /chat/name of room
+# requires login
+
+# /user/<username>
+# redirect to user profile
+# requires login
+
+# /login
 
 
 # Start The App
